ec8291d40811a059c5759e94a2b61f786be96b71
theprithwisingh <prithwisingh77@gmail.com>
2024-03-10 20:42:21 +0530

    08:42 10-3-24

--- Problem11.cpp
+++ Problem11.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the num: ";
+    cin >> n;
+    int a = 1, b=1,sum=0;
+        for (int i = 1; i <= n-2; i++)
+        {
+            sum = a + b;
+            a=b;
+            b=sum;
+        }
+        cout << b;
+}
 
