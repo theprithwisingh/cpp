4369b873b3e9b1dd77336f1154a2babb52ef82d6
theprithwisingh <prithwisingh77@gmail.com>
2024-03-11 11:58:50 +0530

    save

---
+++ Problem12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the num: ";
+    cin >> n;
+     int a=0;
+     int b=1;
+     int nextT=0;
+     for (int i = 0; i <= n; i++)
+     {
+        if (i==1){
+         cout<<a<<", ";
+         continue;
+        }
+        if (i==1){
+         cout<<a<<", ";
+         continue;
+        }
+        nextT=a+b;
+        a=b;
+        b=nextT;
+
+        cout<<nextT<<", ";
+     }
+     return 0;
+}
+
