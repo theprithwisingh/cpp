508a268c918e240ad8ac405a87ad65ad148f4a0d
theprithwisingh <prithwisingh77@gmail.com>
2024-03-16 20:07:09 +0530

    show

--- 1-chapter-loop-pattern/Problem20.cpp
+++ 1-chapter-loop-pattern/Problem20.cpp
@@ -8,10 +8,15 @@ int main(){
 
     for (int i = 1; i <=n; i++){
         for (int j = 1; j <= i; j++)
-        {           
+        {        
         cout<<j;
         }
         cout<<endl;
     }
     
 }
+//1
+//12
+//123
+//1234
+//12345
---
+++ 1-chapter-loop-pattern/Problem26.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the name : ";
+cin>>n;
+int k = 1;
+for (int i = 1; i <= n; i++)
+{
+    cout<<i;
+}
+}
