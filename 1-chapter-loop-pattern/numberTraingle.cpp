f7656ac057fdba6687f3aa7237a0ff92f1b09ae8
theprithwisingh <prithwisingh77@gmail.com>
2024-03-24 18:56:40 +0530

    Number's traingle

---
+++ 1-chapter-loop-pattern/numberTraingle.cpp
@@ -0,0 +1,22 @@
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+//11 12 13 14 15
+#include <iostream>
+using namespace std;
+int main() {
+    int r;
+    cout<<"enter the number rows :";
+    cin>>r;
+    int number = 1;
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << number << " ";
+            // number++;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
