3886594954ac0e553097a092b5c0ef921ce009a2
theprithwisingh <prithwisingh77@gmail.com>
2024-03-28 23:23:03 +0530

    11:22

---
+++ 3-Pointer/revisitingVariable.cpp
@@ -0,0 +1,19 @@
+// Adress of a Variable -> &
+#include<iostream>
+using namespace std;
+int main(){
+    // Adress of a Variable -> &
+    // int x;
+    // char y ='d';
+    // float z =0.06;
+    // cout<<&x<<endl;
+    // cout<<&y<<endl;
+    // cout<<&z;
+
+
+   //
+   int x = 4;
+   int* p = &x;
+   cout<<&x;
+
+}
