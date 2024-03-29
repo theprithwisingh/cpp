806e78c86e56a9af265f534d82f003f02958c995
theprithwisingh <prithwisingh77@gmail.com>
2024-03-29 21:17:31 +0530

    save

---
+++ 2-function/swapWithExtraVar.cpp
@@ -0,0 +1,30 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b; 
+    a = a + b;
+    b = a - b;
+    a = a - b;
+cout<<a<<" "<<b; 
+}
+*/
+
+//function
+#include<iostream>
+using namespace std;
+
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+};
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    swap(a, b);
+    cout << a << "  " << b;
+    return 0;
+}
---
+++ 3-Pointer/syntaxErrorProblem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /* 
+    // this right 
+    int x = 8;
+    int y =9;
+    int* p1 = &x, p2 = y;
+    cout<<*p1 + p2;
+    */
+
+    
+    // this is wrong
+    int x = 8;
+    int y =9;
+    int* p1 = &x, *p2 = y;
+    cout<<*p1 + p2;
+    
+}
