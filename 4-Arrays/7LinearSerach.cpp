7cafc9286d1e0f970e6a6946dfa647f02b6cc244
theprithwisingh <prithwisingh77@gmail.com>
2024-04-01 23:26:21 +0530

    array

---
+++ 4-Arrays/7LinearSerach.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+
+    int arr[n];
+    //input
+    for (int i = 0; i <= n-1; i++)
+    {
+       cin>>arr[i];
+    }
+
+    int x;
+    cout<<"Enter the element you want to search : ";
+    cin>>x;
+    //check mark
+    bool flag = false;//not present
+    for (int i = 0; i <=n-1; i++)
+    {
+        if (arr[i]==x) flag = true;
+
+    }
+    if (flag==true) cout<<"element found";
+    else cout<<"not element found";  
+}
