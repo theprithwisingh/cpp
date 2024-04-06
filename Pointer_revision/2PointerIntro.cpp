3718b45fc6ed44da56895f985b4533bd7b45cafc
theprithwisingh <prithwisingh77@gmail.com>
2024-04-06 23:35:28 +0530

    pointer

---
+++ Pointer_revision/2PointerIntro.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    int* ptr = &x;
+    cout<<ptr<<endl;
+    cout<<&x;
+}
---
+++ Pointer_revision/revisitingPointer.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<sizeof(int)<<endl;//4
+    cout<<sizeof(char)<<endl;//1
+    cout<<sizeof(bool)<<endl;//1
+    cout<<sizeof(float)<<endl;//4
+    cout<<sizeof(double)<<endl;//4
+    cout<<sizeof(long)<<endl;//4
+
+}
