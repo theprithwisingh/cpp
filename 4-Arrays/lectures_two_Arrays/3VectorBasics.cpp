0c235d13743726c733ef2f607a6a6af16face879
theprithwisingh <prithwisingh77@gmail.com>
2024-04-06 21:33:26 +0530

    array

---
+++ 4-Arrays/lectures_two_Arrays/3VectorBasics.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(0);
+    cout<<v[0]<<" ";
+    cout<<v[1]<<" ";
+    cout<<v[2]<<" ";
+    cout<<v[3]<<" ";
+}
