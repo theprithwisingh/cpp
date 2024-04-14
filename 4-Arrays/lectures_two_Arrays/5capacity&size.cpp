2f467e8c9e49920881dfef3bc1817af25fe56235
theprithwisingh <prithwisingh77@gmail.com>
2024-04-14 23:32:22 +0530

    array

---
+++ 4-Arrays/lectures_two_Arrays/5capacity&size.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(6);//1 1
+    v.push_back(1);//2 2
+    v.push_back(9);//3 4
+    v.push_back(0);//4 4
+    v.push_back(6);//5 8
+    v.push_back(1);//6 8
+    v.push_back(9);//7 8
+    v.push_back(0);//8 8
+    v.push_back(6);//9 16
+    v.push_back(1);//10 16
+    v.push_back(9);//11 16
+    v.push_back(0);//12 16
+    v.push_back(6);//13 16
+    v.push_back(1);//14 16
+    v.push_back(9);//15 16
+    v.push_back(0);//16 16
+    v.push_back(0);//17 32
+    cout<<"Size is : "<<v.size()<<endl; //17
+    cout<<"Capacity/box is : "<<v.capacity()<<endl;//32
+
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+    cout<<"Size is : "<<v.size()<<endl; //13
+    cout<<"Capacity/box is : "<<v.capacity()<<endl;//32
+}    
