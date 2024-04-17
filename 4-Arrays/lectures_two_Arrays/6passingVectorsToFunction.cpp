a8b6ea1f850977bfeeaf54b5eef9bba89fdd6f8d
theprithwisingh <prithwisingh77@gmail.com>
2024-04-17 23:39:13 +0530

    array

---
+++ 4-Arrays/lectures_two_Arrays/6passingVectorsToFunction.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void change(vector<int>a){
+a[0]=100;
+for (int i = 0; i < a.size(); i++)
+ {
+    cout<<a[i]<<" ";
+ }
+  cout<<endl;
+}
+int main(){
+    vector<int>v;
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(6);
+    v.push_back(1);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    change(v);
+    for(int i = 0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+}
