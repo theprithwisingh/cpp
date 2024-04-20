b61811733ac127411fdbcbe970442e06e8b7049b
theprithwisingh <prithwisingh77@gmail.com>
2024-04-20 21:26:13 +0530

    sorting in array

---
+++ 4-Arrays/lectures_two_Arrays/7vectorAtSort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(9);
+    v.push_back(0);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(5);
+    v.push_back(4);
+    v.push_back(3);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v.at(i)<<" ";
+    }
+    cout<<endl;
+    //sort
+    sort(v.begin(), v.end());
+        for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v.at(i)<<" ";
+    }
+}
