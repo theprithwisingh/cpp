8fd86d844d6ed24cbe63c5500047d2019228b6f8
theprithwisingh <prithwisingh77@gmail.com>
2024-05-04 21:02:18 +0530

    array sorting que

--- 4-Arrays/lectures_three_Arrays/sortZeroAndOnes.cpp
+++ 4-Arrays/lectures_three_Arrays/sortZeroAndOnes.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -10,6 +11,8 @@ for (int i = 0; i < n; i++)
     if (v[i]==0) noz++;
      else noo++;
 }
+
+//filling elements
 for (int i = 0; i < n; i++)
 {
 if (i<noz) v[i]=0;
@@ -27,6 +30,11 @@ vector<int>v;
     v.push_back(1);
     v.push_back(0);
     v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
 
     for (int i = 0; i < v.size(); i++)
     {
@@ -35,6 +43,57 @@ vector<int>v;
     cout<<endl;
 
     sort01(v);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+*/ 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+void sorting(vector<int>& v){
+int n = v.size();
+int i = 0;
+int j = n-1;
+while(i<j){
+    if(v[j]==1) j--;
+    if(v[i]==0) i++;
+    if (v[i]==1&&v[j]==0){
+            v[i]=0;
+            v[j]=1;
+            i++;
+            j--;
+   }
+  }
+}
+
+int main(){
+vector<int>v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    sorting(v);
 
     for (int i = 0; i < v.size(); i++)
     {
