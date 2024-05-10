3dba71d11fb8966ce1ab49712467125297c49cc4
theprithwisingh <prithwisingh77@gmail.com>
2024-05-10 23:27:09 +0530

    array

---
+++ 4-Arrays/arrayProblem/pickValue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+        if ((i == 0 || nums[i] >= nums[i - 1]) 
+                 && (i == n - 1 || nums[i] >= nums[i + 1])) {
+            return i;
+        }
+    }
+    return -1; // If no peak element is found
+}
+
+int main() {
+    std::vector<int> nums = {1, 2, 3, 1};
+    int peakIndex = findPeakElement(nums);
+    if (peakIndex != -1) {
+        std::cout << "Peak element found at index " << peakIndex << ": " << nums[peakIndex] << std::endl;
+    } else {
+        std::cout << "No peak element found." << std::endl;
+    }
+    return 0;
+}
