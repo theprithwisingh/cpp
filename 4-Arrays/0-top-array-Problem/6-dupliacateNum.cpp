dd09c678a7b27795973855a2f912ed36f59c771a
theprithwisingh <prithwisingh77@gmail.com>
2024-08-27 23:30:18 +0530

    status

--- 4-Arrays/0-top-array-Problem/6-dupliacateNum.cpp
+++ 4-Arrays/0-top-array-Problem/6-dupliacateNum.cpp
@@ -12,9 +12,6 @@ vector<int> removeDuplicate(vector<int> arr1) {
 
     // Traverse the sorted vector and add only unique elements to result
     for (int i = 0; i < arr1.size(); i++) {
-        // If it's the first element or current element is not equal to the previous element, add it to result
-        //{12, 11, 23, 12, 11,14,15,16,9}
-        //{9 11 11 12 12 14 15 16 23}
         if (i==0 || arr1[i] != arr1[i - 1]) {
             result.push_back(arr1[i]);
         }
--- JavaScript/duplicateRemove.js
+++ JavaScript/duplicateRemove.js
@@ -2,5 +2,8 @@ function removeDuplicates(str1,str2) {
    const sortedStr1=str1.split('').sort().join();
    const sortedStr2=str2.split('').sort().join();
    console.log(sortedStr1,"radhe radhe",sortedStr2);
+
+   const removeSameChar1 = sortedStr1.
+   const removeSameChar2 = sortedStr2.
 }
 console.log(removeDuplicates("prithwi","dipika"));
