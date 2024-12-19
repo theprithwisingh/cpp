e8bb5db8654858be45bd53c3f4d64ca1354eed0d
theprithwisingh <prithwisingh77@gmail.com>
2024-12-19 23:09:06 +0530

    recursion

---
+++ 10-Recursion/recursion-2/Q1recursionLoop.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int arr[],int n, int idx){
+     if(idx==n) return;
+     cout<<arr[idx]<<" ";
+     print(arr,n,idx+1);
+}
+int main(){
+    int arr[] = {1,2,3,4,5,6,7};
+    int n =sizeof(arr)/sizeof(arr[0]);
+    print(arr,n,0);
+}
---
+++ 10-Recursion/recursion-2/Q3recursionMaxElem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <climits> 
+using namespace std;
+void maxElem(int arr[], int n, int idx, int max){
+    if(idx==n){
+      cout<<max;
+      return;
+    }
+    if(arr[idx]>max) max=arr[idx];
+    maxElem(arr,n,idx+1,max);
+}
+int main(){
+    int arr[]={1,2,334,5456,67653,45454};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    maxElem(arr,n,0,INT_MIN);
+}
