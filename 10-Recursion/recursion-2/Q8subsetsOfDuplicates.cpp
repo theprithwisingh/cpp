7a99f9d87d4f5325eeb1766ec7226a71fdcc3462
theprithwisingh <prithwisingh77@gmail.com>
2024-12-23 17:05:51 +0530

    recursion

---
+++ 10-Recursion/recursion-2/Q8subsetsOfDuplicates.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void storeSubset(string ans, string original, vector<string>& v, bool flag) {
+    if (original == "") {
+        v.push_back(ans);
+        return;
+    }
+    
+    char ch = original[0];
+    
+    if (original.length() == 1) {
+        if (flag == true) storeSubset(ans + ch, original.substr(1), v, true);
+        storeSubset(ans, original.substr(1), v, true);
+        return;
+    }
+
+    char dh = original[1];
+    
+    if (ch == dh) { // duplicate
+        if (flag == true) storeSubset(ans + ch, original.substr(1), v, true);
+        storeSubset(ans, original.substr(1), v, false);
+    } else { // no duplicate
+        if (flag == true) storeSubset(ans + ch, original.substr(1), v, true);
+        storeSubset(ans, original.substr(1), v, true);
+    }
+}
+
+int main() {
+    string str = "aabbcc";
+    vector<string> v; // Corrected to vector<string>
+    storeSubset("", str, v, true);
+    
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
